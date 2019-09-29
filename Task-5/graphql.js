0298e71ae8f52d4edfbb01887081ea8bbdc38d45
ahv15 <52852877+ahv15@users.noreply.github.com>
2019-09-29 23:16:54 +0530

    Create graphql.js

---
+++ Task-5/graphql.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const endpoint = 'https://api.github.com/graphql'
+const token = ' 5a43ae4c62c3db0efe50aece73d1882d4dffc38f'
+const oauth = {Authorization: 'bearer ' + token}
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question(`Enter users id`, (user) => {
+  readline.close()
+})
+const query = '{' +
+                'repositoryOwner(login: "${user}") { ' +
+                  '... on User {' +
+                         'name, '+
+                          'avatarUrl, '+
+                            'bio, '+
+                             '}' +
+                            '}' +
+                           '}' 
+                    '
+
+axios.post(endpoint, {query: query}, {headers: oauth})
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log('Couldnt find username');
+  });
